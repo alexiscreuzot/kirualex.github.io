888b95256884e5615724f4bb22c8eb09907bcc5e
Alex <kirualex@gmail.com>
2013-04-23 22:13:27 +0200

    animation on menu selection

--- js/AppController.js
+++ js/AppController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-myApp.controller('NavbarController', function NavbarController($scope, $location) {
+myApp.controller('AppController', function($scope, $location) {
 
   $scope.routeIs = function(routeName) {
     return $location.path() === routeName;
