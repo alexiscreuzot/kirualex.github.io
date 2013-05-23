ab1de2c22233ad8252100f8423ddd3d0cf1e3cf4
Alex <kirualex@gmail.com>
2013-05-23 22:47:38 +0200

    fix footer + improve menus

--- js/AppController.js
+++ js/AppController.js
@@ -4,6 +4,12 @@ myApp.controller('AppController', function($scope, $location) {
 
   $scope.routeIs = function(routeName) {
     return $location.path() === routeName;
-  };
+};
+
+$scope.menus =
+[{link:"", icon:"user"},
+{link:"lab", icon:"beaker"},
+{link:"thoughts", icon:"bookmark"},
+{link:"contact", icon:"envelope-alt"}];
 
 });
