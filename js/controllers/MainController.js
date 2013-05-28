18acc0ff9d18d1335700ce3abb43bdf40df7a581
Alexis Creuzot <alexis@mobizel.com>
2013-05-28 10:13:44 +0200

    loader + entrance

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -51,12 +51,13 @@ myApp.controller('MainController',function($scope, $http, $routeParams){
     // Scribble detail
     if($routeParams.scribbleId){
         $scope.scribble = $scope.scribbles[$routeParams.scribbleId];
-
         $http.get('scribbles/'+$routeParams.scribbleId+'.md').success(function(data) {
           //$scope.scribble.content = data;
           var dataToParse = {text:data};
           $http.post('https://api.github.com/markdown', angular.toJson(dataToParse)).success(function(parsedData) {
               $scope.scribble.content = parsedData;
+              $('.loader').addClass('hidden');
+              $('.article').addClass('displayed');
           });
       });
     }
