5a24eea74388a7d67210864fe01e5ba46343bf40
Alexis Creuzot <alexis@mobizel.com>
2013-05-31 12:25:49 +0200

    add status bar library

--- js/controllers/LabController.js
+++ js/controllers/LabController.js
@@ -17,16 +17,20 @@ myApp.controller('LabController',function($scope, $http, $routeParams){
         name:"Libraries",
         content:
         [{
+            name:"KAStatusBar",
+            url:"http://alexiscreuzot.com/KAStatusBar/",
+            description:"An ultra-light status bar status bar notification/alert library for iOS"
+        },{
             name:"KALayoutHelper",
-            url:"http://kirualex.github.io/KALayoutHelper",
+            url:"http://alexiscreuzot.com/KALayoutHelper",
             description:"A work in progress aiming to add a layout mechanism (similar to android) to views in iOS"
         },{
             name:"KASlideShow",
-            url:"http://kirualex.github.io/KASlideShow",
+            url:"http://alexiscreuzot.com/KASlideShow",
             description:"Light slideshow library for iOS "
         },{
             name:"KANibHelper",
-            url:"http://kirualex.github.io/KANibHelper",
+            url:"http://alexiscreuzot.com/KANibHelper",
             description:"Ease up iOS Nib management and improve app maintanability"
         }]
     },{
@@ -34,7 +38,7 @@ myApp.controller('LabController',function($scope, $http, $routeParams){
         content:
         [{
             name:"ColourLove",
-            url:"http://kirualex.github.io/ColourLove",
+            url:"http://alexiscreuzot.com/ColourLove",
             description:"This project only purpose is to provide brief, clean and readable code in a dummy application. It is heavily library based and MVC oriented and shows mecanisms like downloading, storing and displaying data from a webservice."
         }]
     }];
