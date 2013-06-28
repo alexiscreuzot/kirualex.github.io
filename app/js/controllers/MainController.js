73bf55c0de1d37eb8a236a6a5c59b33281bf2601
Alex <kirualex@gmail.com>
2013-06-28 18:46:52 +0200

    style enhancements

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -3,8 +3,10 @@
 // declare controller to myApp
 myApp.controller('MainController',function($scope, $http, $routeParams){
 
+
     $scope.perso = {
         mail:"alexis.creuzot@gmail.com",
+        gravatar:"http://www.gravatar.com/avatar/e5fe13cbbb92ae5b473a25cc95222387?s=100",
         github:"https://github.com/kirualex",
         stack_overflow:"http://stackoverflow.com/users/421786/kirualex",
         hn:"https://news.ycombinator.com/user?id=kirualex",
@@ -12,6 +14,17 @@ myApp.controller('MainController',function($scope, $http, $routeParams){
         osrc:"http://osrc.dfm.io/kirualex",
         isAvailable:false
     };
+
+    var image = $('.avatar img').attr('src', $scope.perso.gravatar);
+    if (! image.prop('complete')) {
+        image.load(function () {
+            $('.avatar').toggleClass('animate');
+            $('.avatar').css('margin', '40px 0 30px');
+        });
+    }else{
+        $scope.imageLoaded = true;
+        $('.avatar').css('margin', '30px');
+    }
 
     // Ready
     $scope.htmlReady();
